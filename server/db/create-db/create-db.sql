81fc4f73aee17281344cefb6ae9ae9949878277b
Martin Zlatev <martin.zlatew@gmail.com>
2022-06-12 13:53:59 +0300

    broken upload image option

--- server/db/create-db/create-db.sql
+++ server/db/create-db/create-db.sql
@@ -8,17 +8,27 @@ CREATE DATABASE IF NOT EXISTS `alumni_album` DEFAULT CHARACTER SET utf8mb4 COLLA
 USE `alumni_album`;
 
 
-CREATE TABLE `users` (
+-- CREATE TABLE `users` (
+--   `id` int(11) UNSIGNED NOT NULL,
+--   `name` varchar(30) NOT NULL,
+--   `email` varchar(50) NOT NULL,
+--   `password` varchar(100) NOT NULL,
+--   `course` int NOT NULL,
+--   `major` varchar(30) NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `images` (
   `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `course` int NOT NULL,
-  `major` varchar(30) NOT NULL
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `image` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- ALTER TABLE `users`
+--   ADD PRIMARY KEY (`id`),
+--   ADD UNIQUE KEY `email` (`email`),
+--   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
-ALTER TABLE `users`
+ALTER TABLE `images`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
+  ADD CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
