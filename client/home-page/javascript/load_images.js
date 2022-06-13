63e97581f35993d8e8db8135c1732fe3dac59350
Martin Zlatev <martin.zlatew@gmail.com>
2022-06-13 21:53:35 +0300

    fix displaying galleries

--- client/home-page/javascript/load_images.js
+++ client/home-page/javascript/load_images.js
@@ -11,11 +11,20 @@ async function loadImages(){
     
     for( const image of data){
         const div= document.createElement('div');
-        div.classList.add('my-image');
+        const div2=document.createElement('div');
+        const div3=document.createElement('div');
+        div.classList.add('album-image');
+        div2.classList.add('user-album-info')
+        div3.classList.add('user-album-image')
+        const name = image["name"];
+        div2.innerHTML=name;
         const img = document.createElement('img');
         img.src= `../../server/images/${image["path"]}`
-        div.appendChild(img);
-        responseDiv.appendChild(div);  
+        div3.appendChild(img);
+        div.appendChild(div3);
+        div.appendChild(div2);
+        responseDiv.appendChild(div); 
+         
     }
     
 }
